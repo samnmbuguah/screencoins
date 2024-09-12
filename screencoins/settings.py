233a13031bb7c9a3ea73f9dd30b1d64833b49ce9
Samuel Nyambura <samnmbuguah@gmail.com>
2024-09-12 01:43:40 +0000

    🚀 Refactor settings to add allowed hosts

--- screencoins/settings.py
+++ screencoins/settings.py
@@ -27,10 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-s0b0bjjyyodh$_#%e5+3!u6ry*ivbq-3a6j(x7r=cg_i@!r^^b')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'True') == 'True'
+DEBUG = os.getenv('DEBUG', 'True').lower() in ('true', '1', 't')
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost 127.0.0.1 [::1]').split()
 
 # Application definition
 
