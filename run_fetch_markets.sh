419ff8a6367ac961a8317a94e54ecdb12f62c307
Samuel Nyambura <samnmbuguah@gmail.com>
2024-09-12 09:23:03 +0000

    🔨 Refactor run_fetch_markets.sh to include commands for fetching markets and updating value area

--- run_fetch_markets.sh
+++ run_fetch_markets.sh
@@ -6,5 +6,8 @@ source myenv/bin/activate
 # Navigate to the Django project directory (current directory)
 cd "$(dirname "$0")"
 
-# Run the Django management command
+# Run the Django management command to fetch markets
 python manage.py fetch_markets
+
+# Run the Django management command to update value area
+python manage.py update_value_area
