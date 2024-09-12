096bc6af97782b1750a57feca39b138ae10de5cc
Samuel Nyambura <samnmbuguah@gmail.com>
2024-09-12 09:23:19 +0000

    🗃️ Refactor models.py to add ValueAreaResult model

--- screener/models.py
+++ screener/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class ValueAreaResult(models.Model):
+    symbol = models.CharField(max_length=50)
+    current_price = models.FloatField()
+    vah = models.FloatField()
+    val = models.FloatField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.symbol
