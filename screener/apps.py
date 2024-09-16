155b188bf48407eeaea366b548dc2b62420cc9f0
Samuel <samnmbuguah@gmail.com>
2024-09-16 07:12:49 +0300

    ✨ Add initialization commands to run on server start

--- screener/apps.py
+++ screener/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class ScreenerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'screener'
+    
+    def ready(self):
+        # Run the commands once when the server starts
+        call_command('fetch_markets')
+        call_command('update_value_area')
