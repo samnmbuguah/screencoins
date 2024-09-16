971f1407640fb2d453e2db7bd85a1bf2a1bcb943
Samuel <samnmbuguah@gmail.com>
2024-09-16 07:24:30 +0300

    🐛 Refactor apps.py to remove unused 'screener' app and add initialization commands to run on server start

--- screener/apps.py
+++ screener/apps.py
@@ -1,10 +1,10 @@
 from django.apps import AppConfig
-
+from django.core.management import call_command
 
 class ScreenerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'screener'
-    
+
     def ready(self):
         # Run the commands once when the server starts
         call_command('fetch_markets')
