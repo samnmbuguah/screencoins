80e0624c338a236b9e71a0fd16a3b028ab5203ea
Samuel <samnmbuguah@gmail.com>
2024-09-16 08:40:57 +0300

    🔥 Refactor apps.py to remove unused code

--- screener/apps.py
+++ screener/apps.py
@@ -1,6 +1,4 @@
 from django.apps import AppConfig
-from django.core.management import call_command
-
 class ScreenerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'screener'
